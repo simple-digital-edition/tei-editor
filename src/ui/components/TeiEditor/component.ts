6b8afbb56ad583033ab43cc317839c818b27cf25
Mark Hall <mark.hall@informatik.uni-halle.de>
2018-11-20 10:45:19 +0100

    Action test.

--- src/ui/components/TeiEditor/component.ts
+++ src/ui/components/TeiEditor/component.ts
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class TeiEditor extends Component {
+    public setBlockAttribute(attribute, value, ev) {
+        ev.preventDefault();
+    }
 }
