7b37f415c447cccc7fa495a0ec3e2f8560288803
Mark <Mark.Hall@work.room3b.eu>
2018-11-23 21:17:01 +0100

    Load from file

--- src/ui/components/TeiEditor/component.ts
+++ src/ui/components/TeiEditor/component.ts
@@ -4,4 +4,24 @@ export default class TeiEditor extends Component {
     public setBlockAttribute(attribute, value, ev) {
         ev.preventDefault();
     }
+
+    public loadFile(ev) {
+        ev.preventDefault();
+        let fileSelector = document.createElement('input');
+        fileSelector.setAttribute('type', 'file');
+        fileSelector.setAttribute('class', 'hidden');
+        document.querySelector('body').append(fileSelector);
+        fileSelector.click();
+        fileSelector.addEventListener('change', function(ev) {
+            let files = ev.target.files;
+            if (files.length > 0) {
+                let reader = new FileReader();
+                reader.onload = (ev) => {
+                    // ev.target.result holds the file
+                }
+                reader.readAsText(files[0]);
+            }
+            fileSelector.remove();
+        });
+    }
 }
