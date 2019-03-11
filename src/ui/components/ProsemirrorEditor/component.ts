e43ce19156b88b99cba003fe1a67fac9c46960d1
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-03-11 09:36:01 +0000

    Refactored out the prosemirror editor

---
+++ src/ui/components/ProsemirrorEditor/component.ts
@@ -0,0 +1,211 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/component';
+import { ensureGuid, HasGuid } from '@glimmer/util';
+import {baseKeymap, setBlockType, toggleMark} from 'prosemirror-commands';
+import {undo, redo, history} from 'prosemirror-history';
+import {keymap} from 'prosemirror-keymap';
+import {Schema} from 'prosemirror-model';
+import {EditorState} from 'prosemirror-state';
+import {Transform} from 'prosemirror-transform';
+import {EditorView} from 'prosemirror-view';
+
+/**
+ * Adds an item into an array, treating the array as a set. If the item to add is already contained in the array,
+ * then nothing is changed.
+ */
+function addToSet(set, item) {
+    let found = false;
+    set.forEach((existing) => {
+        if (existing === item) {
+            found = true;
+        }
+    });
+    if (!found) {
+        set.push(item);
+    }
+}
+/**
+ * Returns a list of active mark names
+ */
+function getMarks(state) {
+    let selection = state.selection;
+    let marks = [];
+    if(selection.from === selection.to) {
+        // Get marks at the current cursor position
+        if(state.doc.nodeAt(selection.from)) {
+            state.doc.nodeAt(selection.from).marks.forEach((mark) => {
+                addToSet(marks, mark);
+            });
+        }
+        // Add marks from the previous cursor position if they are inclusive
+        if(state.doc.nodeAt(selection.from - 1)) {
+            state.doc.nodeAt(selection.from - 1).marks.forEach((mark) => {
+                if(mark.type.spec.inclusive || mark.type.spec.inclusive === undefined) {
+                    addToSet(marks, mark);
+                }
+            });
+        }
+        // Add stored marks
+        if(state.storedMarks) {
+            state.storedMarks.forEach((mark) => {
+                addToSet(marks, mark);
+            });
+        }
+    } else {
+        // Add all marks between the selection markers
+        state.doc.nodesBetween(selection.from, selection.to, (node) => {
+            node.marks.forEach((mark) => {
+                addToSet(marks, mark);
+            });
+        });
+    }
+    return marks;
+}
+
+/**
+ * Gets a list of nodes from the current selection.
+ */
+function getBlockHierarchy(state) {
+    let selection = state.selection
+    let blocks = []
+    for(let idx = 0; idx < selection.$anchor.path.length; idx++) {
+        if(typeof(selection.$anchor.path[idx]) === 'object') {
+            let node_type = selection.$anchor.path[idx].type
+            if(node_type.name !== 'doc') {
+                blocks.splice(0, 0, selection.$anchor.path[idx])
+            }
+        }
+    }
+    return blocks
+}
+
+export default class ProsemirrorEditor extends Component implements HasGuid {
+    _guid: number = null;
+    sourceText: object = null;
+    editorView: EditorView = null;
+    @tracked status: object = null;
+    schema: Schema = null;
+
+    // Life-cycle handlers
+
+    /**
+     * Upon insertion of the component, initialise the Prosemirror instance.
+     */
+    didInsertElement() {
+        this.schema = new Schema(this.args.schema);
+        let state = EditorState.create({
+            schema: this.schema,
+            doc: null,
+            plugins: [
+                history(),
+                keymap({
+                    'Mod-z': undo,
+                    'Mod-y': redo
+                }),
+                keymap(baseKeymap)
+            ]
+        });
+        let component = this;
+        component.editorView = new EditorView(document.querySelector('#' + this.prosemirrorId), {
+            state,
+            dispatchTransaction(transaction) {
+                let newState = component.editorView.state.apply(transaction);
+                component.stateChange(newState);
+                component.editorView.updateState(newState);
+            }
+        });
+    }
+
+    /**
+     * Upon updating of the text in the arguments, load the text into the editor.
+     */
+    didUpdate() {
+        if (this.args.text !== this.sourceText) {
+            this.sourceText = this.args.text;
+            let state = EditorState.create({
+                schema: this.schema,
+                doc: this.schema.nodeFromJSON(this.sourceText),
+                plugins: [
+                    history(),
+                    keymap({
+                        'Mod-z': undo,
+                        'Mod-y': redo
+                    }),
+                    keymap(baseKeymap)
+                ]
+            });
+            this.editorView.updateState(state);
+        }
+    }
+
+    // Computed properties
+
+    /**
+     * Generate a unique identifier for the editor.
+     */
+    get prosemirrorId() {
+        return 'tei-editor-prosemirror-' + ensureGuid(this);
+    }
+
+    // Action handleres
+
+    /**
+     * Called when the editor state changes and updates the current blocks and marks.
+     */
+    private stateChange(state) {
+        let status = {
+            block: null,
+            marks: {}
+        };
+        // Determine most specific active block
+        let blocks = getBlockHierarchy(state);
+        blocks.forEach((node) => {
+            if (node.type.isBlock) {
+                status.block = node;
+            }
+        });
+        // Determine all marks
+        let marks = getMarks(state);
+        marks.forEach((mark) => {
+            status.marks[mark.type.name] = mark;
+        });
+        this.status = status;
+    }
+
+    /**
+     * Handles the actions generated by the right-hand editing sidebar.
+     */
+    public menuAction(action, attribute, value, ev) {
+        this.editorView.focus();
+        if (value === 'ev.target.value') {
+            value = ev.target.value;
+        }
+        if (action === 'setBlockType') {
+            setBlockType(this.schema.nodes[value], {})(this.editorView.state, this.editorView.dispatch)
+        } else if (action === 'setBlockAttribute') {
+            let {$from} = this.editorView.state.selection;
+            let attrs = Object.assign({}, $from.parent.attrs);
+            attrs[attribute] = value;
+            setBlockType(this.schema.nodes[$from.parent.type.name], attrs)(this.editorView.state, this.editorView.dispatch)
+        } else if (action === 'toggleBlockAttribute') {
+            let {$from} = this.editorView.state.selection;
+            let attrs = Object.assign({}, $from.parent.attrs);
+            attrs[attribute] = !attrs[attribute];
+            setBlockType(this.schema.nodes[$from.parent.type.name], attrs)(this.editorView.state, this.editorView.dispatch);
+        } else if (action === 'setMarkAttribute') {
+            attribute = attribute.split('.');
+            let marks = getMarks(this.editorView.state);
+            let {$from, $to} = this.editorView.state.selection
+            let transaction = this.editorView.state.tr;
+            transaction.removeMark($from.pos, $to.pos, this.schema.marks[attribute[0]]);
+            if (value && value.trim() !== '') {
+                let attrs = {}
+                attrs[attribute[1]] = value;
+                transaction.addMark($from.pos, $to.pos, this.schema.marks[attribute[0]].create(attrs));
+            }
+            this.editorView.dispatch(transaction);
+        } else if (action === 'toggleMark') {
+            toggleMark(this.schema.marks[attribute])(this.editorView.state, this.editorView.dispatch);
+        }
+    }
+}
--- src/ui/components/TeiEditor/component.ts
+++ src/ui/components/TeiEditor/component.ts
@@ -1,139 +1,23 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/component';
 import { ensureGuid, HasGuid } from '@glimmer/util';
-import {baseKeymap, setBlockType, toggleMark} from 'prosemirror-commands';
-import {undo, redo, history} from 'prosemirror-history';
-import {keymap} from 'prosemirror-keymap';
-import {Schema} from 'prosemirror-model';
-import {EditorState} from 'prosemirror-state';
-import {Transform} from 'prosemirror-transform';
-import {EditorView} from 'prosemirror-view';
 
 import { TEIParser } from './tei';
 import deepclone from '../deepclone/helper';
 import get from '../get/helper';
 import set from '../set/helper';
 
-/**
- * Adds an item into an array, treating the array as a set. If the item to add is already contained in the array,
- * then nothing is changed.
- */
-function addToSet(set, item) {
-    let found = false;
-    set.forEach((existing) => {
-        if (existing === item) {
-            found = true;
-        }
-    });
-    if (!found) {
-        set.push(item);
-    }
-}
-/**
- * Returns a list of active mark names
- */
-function getMarks(state) {
-    let selection = state.selection;
-    let marks = [];
-    if(selection.from === selection.to) {
-        // Get marks at the current cursor position
-        if(state.doc.nodeAt(selection.from)) {
-            state.doc.nodeAt(selection.from).marks.forEach((mark) => {
-                addToSet(marks, mark);
-            });
-        }
-        // Add marks from the previous cursor position if they are inclusive
-        if(state.doc.nodeAt(selection.from - 1)) {
-            state.doc.nodeAt(selection.from - 1).marks.forEach((mark) => {
-                if(mark.type.spec.inclusive || mark.type.spec.inclusive === undefined) {
-                    addToSet(marks, mark);
-                }
-            });
-        }
-        // Add stored marks
-        if(state.storedMarks) {
-            state.storedMarks.forEach((mark) => {
-                addToSet(marks, mark);
-            });
-        }
-    } else {
-        // Add all marks between the selection markers
-        state.doc.nodesBetween(selection.from, selection.to, (node) => {
-            node.marks.forEach((mark) => {
-                addToSet(marks, mark);
-            });
-        });
-    }
-    return marks;
-}
-
-/**
- * Gets a list of nodes from the current selection.
- */
-function getBlockHierarchy(state) {
-    let selection = state.selection
-    let blocks = []
-    for(let idx = 0; idx < selection.$anchor.path.length; idx++) {
-        if(typeof(selection.$anchor.path[idx]) === 'object') {
-            let node_type = selection.$anchor.path[idx].type
-            if(node_type.name !== 'doc') {
-                blocks.splice(0, 0, selection.$anchor.path[idx])
-            }
-        }
-    }
-    return blocks
-}
-
-export default class TeiEditor extends Component implements HasGuid {
-    _guid: number = null;
-    schema: Schema = null;
-    mainTextView: EditorView = null;
-    @tracked status: object = null;
+export default class TeiEditor extends Component {
+    //schema: Schema = null;
     @tracked currentView: string = '#tei-editor-main-text';
     @tracked loaded: boolean = false;
+    @tracked bodyText: object = null;
     @tracked metadata: object = null;
-
-    // Life-cycle handlers
-
-    constructor(options: object) {
-        super(options);
-        this.schema = new Schema(window.teiEditorConfig.schema);
-    }
-
-    /**
-     * Upon insertion of the component, initialise the Prosemirror instance.
-     */
-    didInsertElement() {
-        let state = EditorState.create({
-            schema: this.schema,
-            doc: null,
-            plugins: [
-                history(),
-                keymap({
-                    'Mod-z': undo,
-                    'Mod-y': redo
-                }),
-                keymap(baseKeymap)
-            ]
-        });
-        let component = this;
-        component.mainTextView = new EditorView(document.querySelector('#' + this.prosemirrorId), {
-            state,
-            dispatchTransaction(transaction) {
-                let newState = component.mainTextView.state.apply(transaction);
-                component.stateChange(newState);
-                component.mainTextView.updateState(newState);
-            }
-        });
-    }
 
     // Computed properties
 
-    /**
-     * Generate a unique identifier for the editor.
-     */
-    get prosemirrorId() {
-        return 'teieditor-' + ensureGuid(this);
+    get mainTextEditorConfig() {
+        return window.teiEditorConfig.schema;
     }
 
     /**
@@ -152,70 +36,6 @@ export default class TeiEditor extends Component implements HasGuid {
 
     // Action handlers
 
-    /**
-     * Called when the editor state changes and updates the current blocks and marks.
-     */
-    private stateChange(state) {
-        let status = {
-            block: null,
-            marks: {}
-        };
-        // Determine most specific active block
-        let blocks = getBlockHierarchy(state);
-        blocks.forEach((node) => {
-            if (node.type.isBlock) {
-                status.block = node;
-            }
-        });
-        // Determine all marks
-        let marks = getMarks(state);
-        marks.forEach((mark) => {
-            status.marks[mark.type.name] = mark;
-        });
-        this.status = status;
-    }
-
-    /**
-     * Handles the actions generated by the right-hand editing sidebar.
-     */
-    public menuAction(action, attribute, value, ev) {
-        this.mainTextView.focus();
-        if (value === 'ev.target.value') {
-            value = ev.target.value;
-        }
-        if (action === 'setBlockType') {
-            setBlockType(this.schema.nodes[value], {})(this.mainTextView.state, this.mainTextView.dispatch)
-        } else if (action === 'setBlockAttribute') {
-            let {$from} = this.mainTextView.state.selection;
-            let attrs = Object.assign({}, $from.parent.attrs);
-            attrs[attribute] = value;
-            setBlockType(this.schema.nodes[$from.parent.type.name], attrs)(this.mainTextView.state, this.mainTextView.dispatch)
-        } else if (action === 'toggleBlockAttribute') {
-            let {$from} = this.mainTextView.state.selection;
-            let attrs = Object.assign({}, $from.parent.attrs);
-            attrs[attribute] = !attrs[attribute];
-            setBlockType(this.schema.nodes[$from.parent.type.name], attrs)(this.mainTextView.state, this.mainTextView.dispatch);
-        } else if (action === 'setMarkAttribute') {
-            attribute = attribute.split('.');
-            let marks = getMarks(this.mainTextView.state);
-            let {$from, $to} = this.mainTextView.state.selection
-            let transaction = this.mainTextView.state.tr;
-            transaction.removeMark($from.pos, $to.pos, this.schema.marks[attribute[0]]);
-            if (value && value.trim() !== '') {
-                let attrs = {}
-                attrs[attribute[1]] = value;
-                transaction.addMark($from.pos, $to.pos, this.schema.marks[attribute[0]].create(attrs));
-            }
-            this.mainTextView.dispatch(transaction);
-        } else if (action === 'toggleMark') {
-            toggleMark(this.schema.marks[attribute])(this.mainTextView.state, this.mainTextView.dispatch);
-        }
-    }
-
-    public setBlockAttribute(attribute, value, ev) {
-        ev.preventDefault();
-    }
-
     public loadFile(ev) {
         ev.preventDefault();
         let component = this;
@@ -230,20 +50,7 @@ export default class TeiEditor extends Component implements HasGuid {
                 let reader = new FileReader();
                 reader.onload = (ev) => {
                     let parser = new TEIParser(ev.target.result, window.teiEditorConfig);
-                    let doc = component.schema.nodeFromJSON(parser.body);
-                    let state = EditorState.create({
-                        schema: component.schema,
-                        doc: doc,
-                        plugins: [
-                            history(),
-                            keymap({
-                                'Mod-z': undo,
-                                'Mod-y': redo
-                            }),
-                            keymap(baseKeymap)
-                        ]
-                    });
-                    component.mainTextView.updateState(state);
+                    component.bodyText = parser.body;
                     component.metadata = parser.metadata;
                     component.loaded = true;
                 }
@@ -253,6 +60,9 @@ export default class TeiEditor extends Component implements HasGuid {
         });
     }
 
+    /**
+     * Switch the active view.
+     */
     public setView(view, ev) {
         ev.preventDefault();
         document.querySelector(this.currentView).setAttribute('aria-hidden', 'true');
