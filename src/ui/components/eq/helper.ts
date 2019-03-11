fb22d8ce557e884629574cde399d1bce73e67d02
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-03-11 07:33:02 +0000

    Remove unused code

--- src/ui/components/eq/helper.ts
+++ src/ui/components/eq/helper.ts
@@ -1,7 +1,6 @@
+/**
+ * Compare whether the first two parameters are equal.
+ */
 export default function eq(params) {
-    if (params[1] === 'toggle') {
-        return params[0] !== undefined && params[0] !== null;
-    } else {
-        return params[0] === params[1];
-    }
+    return params[0] === params[1];
 }
